6428d5c585c436f6c526ba36170bf147f48215fe
fahim-cmd <hfahim087@gmail.com>
2020-07-17 01:23:58 +0600

    added, how we can convert feet to mile.

---
+++ feetToMile.js
@@ -0,0 +1,8 @@
+//How can we convert feet to mile.
+
+function feetToMile (feet){
+    const mile = feet/5280;
+    return mile;
+}
+const getFeetToMile = feetToMile(5280);
+console.log(getFeetToMile);
